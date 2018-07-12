ca095968fdc138537b6613498c78661c06a52a44
Anna Shlyaeva <anna.v.shlyaeva@noaa.gov>
2018-07-12 15:55:52 +0000

    Some fixes for CFSR

--- RAD/call_putdate_RAD.py
+++ RAD/call_putdate_RAD.py
@@ -1,4 +1,5 @@
 from putdate_annual_rad import putdate_annual_rad
+from putdate_CFSR_annual_rad import putdate_CFSR_annual_rad
 from create_annual_rad import create_annual_rad
 import os
 
@@ -16,24 +17,15 @@ def call_putdate_RAD( diagpath, modelstream,  date, instrmnt, satlite, outputpat
 
    outfile=outputpath+'/RAD_'+modelstream+'_'+str(datayr)+'_'+instrmnt+'_'+satlite+'_'+region+'.nc'
 
-   print 'outfile=',outfile
-   if os.path.isfile(outfile):
-      if modelstream=='CFSR':
-         from putdate_CFSR_annual_rad import putdate_CFSR_annual_rad
-         putdate_CFSR_annual_rad(diagpath, date, instrmnt, satlite, latrange, outfile)
-      else:
-         putdate_annual_rad(diagpath, date, instrmnt, satlite, latrange, outfile)
-   else:
+   if (not os.path.isfile(outfile)):
       channelsname=instrmnt+'_channels'
       chanimport='from channel_dictionary import '+channelsname
       exec(chanimport)
       channels=eval(channelsname)
-      create_annual_rad(outfile, modelstream, int(datayr), instrmnt, channels, satlite, region)         
-      if modelstream=='CFSR':
-         from putdate_CFSR_annual_rad import putdate_CFSR_annual_rad
-         putdate_CFSR_annual_rad(diagpath, date, instrmnt, satlite, latrange, outfile)
-      else:
-         putdate_annual_rad(diagpath, date, instrmnt, satlite, latrange, outfile)
+      create_annual_rad(outfile, modelstream, int(datayr), instrmnt, channels, satlite, region)
 
-
+   if modelstream=='CFSR':
+      putdate_CFSR_annual_rad(diagpath, date, instrmnt, satlite, latrange, outfile)
+   else:
+      putdate_annual_rad(diagpath, date, instrmnt, satlite, latrange, outfile)
 
--- RAD/putdate_CFSR_annual_rad.py
+++ RAD/putdate_CFSR_annual_rad.py
@@ -4,167 +4,89 @@ import sys, os
 from netCDF4 import num2date, date2num, date2index
 import read_diag
 
-nan=float('nan')
-def putdate_CFSR_annual_rad(diagpath, date, instrmnt, satlite, latrange, outfile):
-   print('INSIDE PUT FXN')
+def putdate_CFSR_annual_rad(diagpath, date, instrmnt, sat, outputpath):
    date10dig=str(date)
- 
-   diaganl_obsfile = diagpath +date10dig+ '/diag_'  +  instrmnt + '_' + satlite + '_anl.' + date10dig
+   datayr = date / 1000000
+
+   diaganl_obsfile = diagpath + '/' + date10dig + '/diag_'  +  instrmnt + '_' + sat + '_anl.' + date10dig
+   if (not os.path.isfile(diaganl_obsfile)):
+      print instrmnt, sat, ' not available for ', date
+      return
+   print 'diag anl file=', diaganl_obsfile
+
    diaganl_rad = read_diag.diag_rad(diaganl_obsfile,endian='big')
    diaganl_rad.read_obs()
-   print 'diag anl file=', diaganl_obsfile
    
-   ###########################
-   
-   ANLnobs_diag = diaganl_rad.nobs
-   ANLlon_diag = diaganl_rad.lon
-   ANLlat_diag = diaganl_rad.lat
-   ANLtime_diag = diaganl_rad.time
-   ANLchan_diag = diaganl_rad.channel
-   fn_diaganl = diaganl_rad.filename
-   npred_diaganl = diaganl_rad.npred
-   hx_diaganl = diaganl_rad.hx
-   hxnobc_diaganl = diaganl_rad.hxnobc
-   biascorr_diaganl = diaganl_rad.biascorr
-   obs_diaganl = diaganl_rad.obs
-   oberr_diaganl = diaganl_rad.oberr
-   biaspred_diaganl = diaganl_rad.biaspred
-   oberr_orig_diaganl = diaganl_rad.oberr_orig
-   used_diaganl = diaganl_rad.used
-   qcmark_diaganl = diaganl_rad.qcmark
+   nobs = diaganl_rad.nobs
+   lat  = diaganl_rad.lat
+   chan = diaganl_rad.channel
+   obs  = diaganl_rad.obs
+   obserr = diaganl_rad.oberr
+   gsi_used = diaganl_rad.used
+   gsi_qcd  = diaganl_rad.qcmark
+   oma_ctrl = obs - diaganl_rad.hx
    #############################
    
-   diagges_obsfile = diagpath +date10dig+ '/diag_'  +  instrmnt + '_' + satlite + '_ges.' + date10dig
+   diagges_obsfile = diagpath +date10dig+ '/diag_'  +  instrmnt + '_' + sat + '_ges.' + date10dig
    diagges_rad = read_diag.diag_rad(diagges_obsfile,endian='big')
    diagges_rad.read_obs()
    print 'ges file=',diagges_obsfile
-   
-   ###########################
-   ###########################
-   GESnobs_diag = diagges_rad.nobs
-   GESlon_diag = diagges_rad.lon
-   GESlat_diag = diagges_rad.lat
-   GEStime_diag = diagges_rad.time
-   GESchan_diag = diagges_rad.channel
-   fn_diagges = diagges_rad.filename
-   npred_diagges = diagges_rad.npred
-   hx_diagges = diagges_rad.hx
-   hxnobc_diagges = diagges_rad.hxnobc
-   biascorr_diagges = diagges_rad.biascorr
-   obs_diagges = diagges_rad.obs
-   oberr_diagges = diagges_rad.oberr
-   biaspred_diagges = diagges_rad.biaspred
-   oberr_orig_diagges = diagges_rad.oberr_orig
-   used_diagges = diagges_rad.used
-   qcmark_diagges = diagges_rad.qcmark
-   ###########################################
-   ###########################################
-   ###########################################
-   ###########################################
-   ###########################################
-   ###########################################
-   obs = obs_diaganl
-   chan_diag = ANLchan_diag
-   lat  = ANLlat_diag
-   latidx = np.logical_and(lat >= np.min(latrange), lat <= np.max(latrange))
-   omf_ctrl = hx_diagges
-   oma_ctrl = hx_diaganl
-   omf_ens  = nan*np.ones(ANLnobs_diag)
-   oma_ens  = nan*np.ones(ANLnobs_diag)
-   gsi_used  = used_diaganl
-   enkf_used = nan*np.ones(ANLnobs_diag)
-   gsi_qcd  = qcmark_diaganl
-   sprd_f =  nan*np.ones(ANLnobs_diag)
-   sprd_a =  nan*np.ones(ANLnobs_diag)
-   obserr = oberr_diaganl
-   biascorr = hxnobc_diagges - hx_diagges
-   ############################################
-   #anndata = Dataset(outfile, 'a')
-   #alldate=anndata['All_Dates']
-   #idate = np.nonzero(alldate[:]==date)[0][0]
-   #anndata['Full_Dates'][idate] = date
-   ###### ['Ncycles','Nchans']
-   #chans = anndata['Channels'][:].tolist()
-   ##print 'lenchan=',len(chans)
-   #for ichan in range(len(chans)):
-   #   chanidx = (chan_diag==chans[ichan])
-   #   #chanidx = (chan_diag==chanlist_diag[ichan])
-   #   #print 'lenlatindx,lenchanindx=',len(latidx),len(chanidx)
-   #   chanlatidx = np.logical_and(chanidx, latidx)
-   #   qcdidx  = (gsi_qcd == 0)
-   #   qidx = np.logical_and(qcdidx, chanlatidx)
-   #   anndata['nobs_all'][idate,ichan]  = len(obs[chanlatidx])
-   #   #anndata['nobs_used'][idate,ichan] = len(obs[idx])
-   #   anndata['nobs_qcd'][idate,ichan] = len(obs[qidx])
-   #   anndata['mean_obs_all'][idate,ichan]  = np.mean(obs[chanlatidx])
-   #   anndata['mean_obs_used'][idate,ichan] = np.mean(obs[qidx])
-   #   anndata['mean_obs_qcd'][idate,ichan] = np.mean(obs[qidx])
-   #   anndata['mean_omf_ctrl'][idate,ichan] = np.mean(omf_ctrl[qidx])
-   #   anndata['mean_oma_ctrl'][idate,ichan] = np.mean(oma_ctrl[qidx])
-   #   anndata['std_omf_ctrl'][idate,ichan]  = np.sqrt(np.mean(omf_ctrl[qidx] ** 2))
-   #   anndata['std_oma_ctrl'][idate,ichan]  = np.sqrt(np.mean(oma_ctrl[qidx] ** 2))
-   #   #del idx
-   #   #del useidx
-   #   useidx = (enkf_used == 1)  ## for enkf use only
-   #   idx = np.logical_and(useidx, chanlatidx)
-   #   anndata['nobs_used'][idate,ichan] = len(obs[idx])
-   #   anndata['mean_omf_ens'][idate,ichan] = np.mean(omf_ens[idx])
-   #   # LEAVING OUT FOR NOW     anndata['mean_oma_ens'][idate,ichan] = np.mean(oma_ens[idx])
-   #   anndata['spread_f'][idate,ichan] = np.sqrt(np.mean(sprd_f[idx]))
-   #   # LEAVING OUT FOR NOW     anndata['spread_a'][idate,ichan] = np.sqrt(np.mean(sprd_a[idx]))
-   #   anndata['spread_obserr_f'][idate,ichan] = np.sqrt(np.mean(sprd_f[idx] + obserr[idx]))
-   #   # LEAVING OUT FOR NOW     anndata['spread_obserr_a'][idate,ichan] = np.sqrt(np.mean(sprd_a[idx] + obserr[idx]))
-   #   anndata['std_omf_ens'][idate,ichan]  = np.sqrt(np.mean(omf_ens[idx] ** 2))
-   #   anndata['mean_biascor'][idate,ichan]=np.mean(biascorr[idx])
-   #   anndata['std_biascor'][idate,ichan]=np.std(biascorr[idx])
-   #anndata.close()
 
-   ############################################
-   anndata = Dataset(outfile, 'a')
-   alldate=anndata['All_Dates']
-   idate = np.nonzero(alldate[:]==date)[0][0]
-   anndata['Full_Dates'][idate] = date
-   ##### ['Ncycles','Nchans']
-   chans = anndata['Channels'][:].tolist()
-   print 'lenannhan=',len(chans)
-   for ichan in range(len(chans)):
-      print('GOT HERE ',ichan)
-      chanidx = (chan_diag==chans[ichan])
-      #chanidx = (chan_diag==chanlist_diag[ichan])
-      #print 'lenlatindx,lenchanindx=',len(latidx),len(chanidx)
-      chanlatidx = np.logical_and(chanidx, latidx)
-      print('chanlatindx=',chanlatidx)
-      qcdidx  = (gsi_qcd == 0)
-      qidx = np.logical_and(qcdidx, chanlatidx)
-      anndata['nobs_all'][idate,ichan]  = len(obs[chanlatidx])
-      #anndata['nobs_used'][idate,ichan] = len(obs[idx])
-      anndata['nobs_qcd'][idate,ichan] = len(obs[qidx])
-      anndata['mean_obs_all'][idate,ichan]  = np.mean(obs[chanlatidx])
-      anndata['mean_obs_used'][idate,ichan] = np.mean(obs[qidx])
-      anndata['mean_obs_qcd'][idate,ichan] = np.mean(obs[qidx])
-      anndata['mean_omf_ctrl'][idate,ichan] = np.mean(omf_ctrl[qidx])
-      anndata['mean_oma_ctrl'][idate,ichan] = np.mean(oma_ctrl[qidx])
-      anndata['std_omf_ctrl'][idate,ichan]  = np.sqrt(np.mean(omf_ctrl[qidx] ** 2))
-      anndata['std_oma_ctrl'][idate,ichan]  = np.sqrt(np.mean(oma_ctrl[qidx] ** 2))
-      #del idx
-      #del useidx
-      print('GOT HERE middle',ichan)
-      useidx = (enkf_used == 1)  ## for enkf use only
-      idx = np.logical_and(useidx, chanlatidx)
-      anndata['nobs_used'][idate,ichan] = len(obs[idx])
-      anndata['mean_omf_ens'][idate,ichan] = np.mean(omf_ens[idx])
-      # LEAVING OUT FOR NOW     anndata['mean_oma_ens'][idate,ichan] = np.mean(oma_ens[idx])
-      anndata['spread_f'][idate,ichan] = np.sqrt(np.mean(sprd_f[idx]))
-      # LEAVING OUT FOR NOW     anndata['spread_a'][idate,ichan] = np.sqrt(np.mean(sprd_a[idx]))
-      anndata['spread_obserr_f'][idate,ichan] = np.sqrt(np.mean(sprd_f[idx] + obserr[idx]))
-      # LEAVING OUT FOR NOW     anndata['spread_obserr_a'][idate,ichan] = np.sqrt(np.mean(sprd_a[idx] + obserr[idx]))
-      anndata['std_omf_ens'][idate,ichan]  = np.sqrt(np.mean(omf_ens[idx] ** 2))
-      anndata['mean_biascor'][idate,ichan]=np.mean(biascorr[idx])
-      anndata['std_biascor'][idate,ichan]=np.std(biascorr[idx])
-      print('GOT HERE end',ichan)
-   print('finished loop')
-   anndata.close()
-   print('leaving putdate function')
+   omf_ctrl = obs - diagges_rad.hx
+   biascorr = diagges_rad.biascorr
+   ###########################################
+
+   regions=['GLOBL','TROPI','NORTH','SOUTH']
+
+   for region in regions:
+     if region=='GLOBL':
+        latrange=[-90, 90]
+     elif region=='TROPI':
+        latrange=[-20,20]
+     elif region=='SOUTH':
+        latrange=[-90,-20]
+     elif region=='NORTH':
+        latrange=[20,90]
+  
+     outfile=outputpath+'/RAD_CFSR_'+str(datayr)+'_'+instrmnt+'_'+sat+'_'+region+'.nc'
+     print outfile
+     latidx = np.logical_and(lat >= np.min(latrange), lat <= np.max(latrange))
+     ############################################
+  
+     anndata = Dataset(outfile, 'a')
+     alldate=anndata['All_Dates']
+     idate = np.nonzero(alldate[:]==date)[0][0]
+     anndata['Full_Dates'][idate] = date
+     ##### ['Ncycles','Nchans']
+     chans = anndata['Channels'][:].tolist()
+     print 'lenannhan=',len(chans)
+     for ichan in range(len(chans)):
+        chanidx    = (chan==chans[ichan])
+        chanlatidx = np.logical_and(chanidx, latidx)
+        qcdidx     = (gsi_qcd == 0)
+        qidx       = np.logical_and(qcdidx, chanlatidx)
+        anndata['nobs_all'][idate,ichan]  = len(obs[chanlatidx])
+        anndata['nobs_qcd'][idate,ichan]  = len(obs[qidx])
+        anndata['mean_obs_all'][idate,ichan]  = np.mean(obs[chanlatidx])
+        anndata['mean_obs_used'][idate,ichan] = np.mean(obs[qidx])
+        anndata['mean_obs_qcd'][idate,ichan]  = np.mean(obs[qidx])
+        anndata['mean_omf_ctrl'][idate,ichan] = np.mean(omf_ctrl[qidx])
+        anndata['mean_oma_ctrl'][idate,ichan] = np.mean(oma_ctrl[qidx])
+        anndata['std_omf_ctrl'][idate,ichan]  = np.sqrt(np.mean(omf_ctrl[qidx] ** 2))
+        anndata['std_oma_ctrl'][idate,ichan]  = np.sqrt(np.mean(oma_ctrl[qidx] ** 2))
+        anndata['nobs_used'][idate,ichan] = len(obs[qidx])
+        anndata['mean_biascor'][idate,ichan]=np.mean(biascorr[qidx])
+        anndata['std_biascor'][idate,ichan]=np.std(biascorr[qidx])
+        # no info from ensemble in CFSR:
+        anndata['mean_omf_ens'][idate,ichan]    = np.nan
+        anndata['mean_oma_ens'][idate,ichan]    = np.nan
+        anndata['spread_f'][idate,ichan]        = np.nan
+        anndata['spread_a'][idate,ichan]        = np.nan
+        anndata['spread_obserr_f'][idate,ichan] = np.nan
+        anndata['spread_obserr_a'][idate,ichan] = np.nan
+        anndata['std_omf_ens'][idate,ichan]     = np.nan
+        anndata['std_oma_ens'][idate,ichan]     = np.nan
+     anndata.close()
 
 
 
