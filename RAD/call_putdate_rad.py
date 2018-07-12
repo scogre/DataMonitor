ac400c4df043bb90b7312fa858863f8e2dd2733b
Anna Shlyaeva <anna.v.shlyaeva@noaa.gov>
2018-07-12 20:33:49 +0000

    Some changes to call_putdate_rad (now script for a particular date), removed a file; fix in the putdate; using channel_dictionary in call_putdate_rad and fillin_cfsr (revisit later)

--- RAD/call_putdate_rad.py
+++ RAD/call_putdate_rad.py
@@ -1,12 +1,27 @@
+from putdate_annual_rad import putdate_annual_rad
 from putdate_CFSR_annual_rad import putdate_CFSR_annual_rad
 from create_annual_rad import create_annual_rad
 import sys
-###python make_rad_annual_CFSR.py 1999 1999 /lfs3/projects/gfsenkf/Scott.Gregory/CFSR/
+import os
+
+print('python call_putdate_rad.py <modelstream> <date>')
+print('This python scripts processes diag files for modelstream (CFSR or year of fv3reanl stream)')
+print(' for date <date>. ')
+print('NOTE: The paths to diags and to output are hardcoded in the script.')
+
+if len(sys.argv) < 2:
+    raise SystemExit('python call_putdate_rad.py <modelstream name> <data year> <output path>')
+modelstream = sys.argv[1]
+date = int(sys.argv[2])
+
+datayr=str(date)[0:4]
+
 regions=['GLOBL','TROPI','NORTH','SOUTH']
 
 
 instrmnts=['airs', 'amsua',  'amsua',  'amsua','amsua', 'amsua', 'amsua', 'amsua', 'amsub', 'amsub', 'amsub', 'atms' , 'avhrr'  , 'avhrr', 'avhrr', 'avhrr', 'avhrr', 'cris', 'hirs2' , 'hirs3', 'hirs3', 'hirs4'   , 'hirs4' , 'iasi'   ,    'mhs',     'mhs', 'mhs' , 'mhs', 'msu', 'seviri', 'sndr', 'sndr']
 satlites=[ 'aqua', 'aqua' ,'metop-a','metop-b','n15'  , 'n16'  , 'n18'  , 'n19'  , 'n15'  , 'n16'  , 'n17'  , 'npp'  , 'metop-a', 'n15'  , 'n16'  , 'n17'  , 'n18'  , 'npp' , 'n14'   , 'n16'  , 'n17'  , 'metop-a' , 'n19'   , 'metop-a','metop-a', 'metop-b', 'n18' , 'n19', 'n14',  'm10'  , 'g08' , 'g11' ]
+
 
 diagpath = '/lfs3/projects/gfsenkf/Scott.Gregory/CFSR/'
 outputpath = '/lfs3/projects/gfsenkf/ashlyaeva/monitor/CFSR/'
@@ -22,15 +37,14 @@ for nn in range(numinst):
    channels=eval(channelsname)
 
    print 'channels_name,channels=', channelsname,channels
-   year = 1999
    for region in regions:
-      outfile=outputpath+'/RAD_CFSR_'+str(year)+'_'+instrmnt+'_'+satlite+'_'+region+'.nc'
-      create_annual_rad(outfile, year, year, instrmnt, channels, satlite, region)
-   for month in range(12):
-     for day in range(1):
-       for hour in [0, 6, 12, 18]:
-         date = year*1000000 + (month+1)*10000 + (day+1)*100 + hour
-         print date
-         putdate_CFSR_annual_rad(diagpath, date, instrmnt, satlite, outputpath)
-   del channels
-
+      outfile=outputpath+'/RAD_'+modelstream+'_'+str(datayr)+'_'+instrmnt+'_'+satlite+'_'+region+'.nc'
+      if (not os.path.isfile(outfile)):
+        print('file ', outfile, ' doesnt exist; creating the file.')
+        create_annual_rad(outfile, modelstream, int(datayr), instrmnt, channels, satlite, region)
+   if modelstream=='CFSR':
+      print ('filling in CFSR data')
+      putdate_CFSR_annual_rad(diagpath, date, instrmnt, satlite, outputpath)
+   else:
+      print('filling in reanalysis data')
+      putdate_annual_rad(diagpath, date, modelstream, instrmnt, satlite, outputpath)
--- RAD/putdate_annual_rad.py
+++ RAD/putdate_annual_rad.py
@@ -4,7 +4,7 @@ import sys, os
 from netCDF4 import num2date, date2num, date2index
 
 
-def putdate_annual_rad(diagpath, date, instrmnt, satlite, latrange, outfile):
+def putdate_annual_rad(diagpath, date, stream, instrmnt, satlite, outputpath): 
    #print 'putdate_annual_rad:',date
    anlcontrolnc_file =  diagpath+'/'+str(date)+'/diag_'+instrmnt+'_'+satlite+'_anl.'+str(date)+'_control.nc4'
    diag_ctrl_a = Dataset(anlcontrolnc_file,'r')
@@ -26,7 +26,6 @@ def putdate_annual_rad(diagpath, date, instrmnt, satlite, latrange, outfile):
       chan_diag[m]=chanlist_diag[chanindx_diag[m]-1]
 
    lat  = diag_ctrl_f['Latitude'][:]
-   latidx = np.logical_and(lat >= np.min(latrange), lat <= np.max(latrange))
 
    omf_ctrl = diag_ctrl_f['Obs_Minus_Forecast_adjusted'][:]
    oma_ctrl = diag_ctrl_a['Obs_Minus_Forecast_adjusted'][:]
@@ -43,45 +42,56 @@ def putdate_annual_rad(diagpath, date, instrmnt, satlite, latrange, outfile):
 
    biascorr = diag_ctrl_f['Obs_Minus_Forecast_unadjusted'][:] - diag_ctrl_f['Obs_Minus_Forecast_adjusted'][:]
 
-   anndata = Dataset(outfile, 'a')
-   alldate=anndata['All_Dates']
-   idate = np.nonzero(alldate[:]==date)[0][0]
-   anndata['Full_Dates'][idate] = date
+   regions=['GLOBL','TROPI','NORTH','SOUTH']
 
-   ##### ['Ncycles','Nchans']
-   chans = anndata['Channels'][:].tolist()
-   #print 'lenchan=',len(chans)
-   for ichan in range(len(chans)):
-      chanidx = (chan_diag==chans[ichan])
-      #chanidx = (chan_diag==chanlist_diag[ichan])
-      #print 'lenlatindx,lenchanindx=',len(latidx),len(chanidx)
-      chanlatidx = np.logical_and(chanidx, latidx)
-      qcdidx  = (gsi_qcd == 0)
-      qidx = np.logical_and(qcdidx, chanlatidx)
-      anndata['nobs_all'][idate,ichan]  = len(obs[chanlatidx])
-      #anndata['nobs_used'][idate,ichan] = len(obs[idx])
-      anndata['nobs_qcd'][idate,ichan] = len(obs[qidx])
-      anndata['mean_obs_all'][idate,ichan]  = np.mean(obs[chanlatidx])
-      anndata['mean_obs_used'][idate,ichan] = np.mean(obs[qidx])
-      anndata['mean_obs_qcd'][idate,ichan] = np.mean(obs[qidx])
-      anndata['mean_omf_ctrl'][idate,ichan] = np.mean(omf_ctrl[qidx])
-      anndata['mean_oma_ctrl'][idate,ichan] = np.mean(oma_ctrl[qidx])
-      anndata['std_omf_ctrl'][idate,ichan]  = np.sqrt(np.mean(omf_ctrl[qidx] ** 2))
-      anndata['std_oma_ctrl'][idate,ichan]  = np.sqrt(np.mean(oma_ctrl[qidx] ** 2))
-      #del idx
-      #del useidx
-      useidx = (enkf_used == 1)  ## for enkf use only
-      idx = np.logical_and(useidx, chanlatidx)
-      anndata['nobs_used'][idate,ichan] = len(obs[idx])
-      anndata['mean_omf_ens'][idate,ichan] = np.mean(omf_ens[idx])
-      # LEAVING OUT FOR NOW     anndata['mean_oma_ens'][idate,ichan] = np.mean(oma_ens[idx])
-      anndata['spread_f'][idate,ichan] = np.sqrt(np.mean(sprd_f[idx]))
-      # LEAVING OUT FOR NOW     anndata['spread_a'][idate,ichan] = np.sqrt(np.mean(sprd_a[idx]))
-      anndata['spread_obserr_f'][idate,ichan] = np.sqrt(np.mean(sprd_f[idx] + obserr[idx]))
-      # LEAVING OUT FOR NOW     anndata['spread_obserr_a'][idate,ichan] = np.sqrt(np.mean(sprd_a[idx] + obserr[idx]))
-      anndata['std_omf_ens'][idate,ichan]  = np.sqrt(np.mean(omf_ens[idx] ** 2))
-      anndata['mean_biascor'][idate,ichan]=np.mean(biascorr[idx])
-      anndata['std_biascor'][idate,ichan]=np.std(biascorr[idx])
-   anndata.close()
+   for region in regions:
+     if region=='GLOBL':
+        latrange=[-90, 90]
+     elif region=='TROPI':
+        latrange=[-20,20]
+     elif region=='SOUTH':
+        latrange=[-90,-20]
+     elif region=='NORTH':
+        latrange=[20,90]
+
+     outfile=outputpath+'/RAD_'+stream+'_'+str(datayr)+'_'+instrmnt+'_'+sat+'_'+region+'.nc'
+     print outfile
+     latidx = np.logical_and(lat >= np.min(latrange), lat <= np.max(latrange))
+
+     anndata = Dataset(outfile, 'a')
+     alldate=anndata['All_Dates']
+     idate = np.nonzero(alldate[:]==date)[0][0]
+     anndata['Full_Dates'][idate] = date
+  
+     ##### ['Ncycles','Nchans']
+     chans = anndata['Channels'][:].tolist()
+     #print 'lenchan=',len(chans)
+     for ichan in range(len(chans)):
+        chanidx    = (chan_diag==chans[ichan])
+        chanlatidx = np.logical_and(chanidx, latidx)
+        qcdidx  = (gsi_qcd == 0)
+        qidx    = np.logical_and(qcdidx, chanlatidx)
+        anndata['nobs_all'][idate,ichan]  = len(obs[chanlatidx])
+        anndata['nobs_qcd'][idate,ichan] = len(obs[qidx])
+        anndata['mean_obs_all'][idate,ichan]  = np.mean(obs[chanlatidx])
+        anndata['mean_obs_used'][idate,ichan] = np.mean(obs[qidx])
+        anndata['mean_obs_qcd'][idate,ichan] = np.mean(obs[qidx])
+        anndata['mean_omf_ctrl'][idate,ichan] = np.mean(omf_ctrl[qidx])
+        anndata['mean_oma_ctrl'][idate,ichan] = np.mean(oma_ctrl[qidx])
+        anndata['std_omf_ctrl'][idate,ichan]  = np.sqrt(np.mean(omf_ctrl[qidx] ** 2))
+        anndata['std_oma_ctrl'][idate,ichan]  = np.sqrt(np.mean(oma_ctrl[qidx] ** 2))
+        useidx = (enkf_used == 1)  ## for enkf use only
+        idx    = np.logical_and(useidx, chanlatidx)
+        anndata['nobs_used'][idate,ichan] = len(obs[idx])
+        anndata['mean_omf_ens'][idate,ichan] = np.mean(omf_ens[idx])
+        # LEAVING OUT FOR NOW     anndata['mean_oma_ens'][idate,ichan] = np.mean(oma_ens[idx])
+        anndata['spread_f'][idate,ichan] = np.sqrt(np.mean(sprd_f[idx]))
+        # LEAVING OUT FOR NOW     anndata['spread_a'][idate,ichan] = np.sqrt(np.mean(sprd_a[idx]))
+        anndata['spread_obserr_f'][idate,ichan] = np.sqrt(np.mean(sprd_f[idx] + obserr[idx]))
+        # LEAVING OUT FOR NOW     anndata['spread_obserr_a'][idate,ichan] = np.sqrt(np.mean(sprd_a[idx] + obserr[idx]))
+        anndata['std_omf_ens'][idate,ichan]  = np.sqrt(np.mean(omf_ens[idx] ** 2))
+        anndata['mean_biascor'][idate,ichan] = np.mean(biascorr[idx])
+        anndata['std_biascor'][idate,ichan]  = np.std(biascorr[idx])
+     anndata.close()
    
 
