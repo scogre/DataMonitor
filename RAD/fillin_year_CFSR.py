2d537da803be38a34790960f36ff6c6dd61d7482
Anna Shlyaeva <anna.v.shlyaeva@noaa.gov>
2018-07-17 15:27:34 +0000

    added script to fill in whole year of cfsr (on jet)

--- RAD/fillin_year_CFSR.py
+++ RAD/fillin_year_CFSR.py
@@ -1,9 +1,18 @@
 from putdate_CFSR_annual_rad import putdate_CFSR_annual_rad
 from create_annual_rad import create_annual_rad
 import sys
-###python make_rad_annual_CFSR.py 1999 1999 /lfs3/projects/gfsenkf/Scott.Gregory/CFSR/
+
+print('python fillin_year_CFSR.py <year>')
+print('This python scripts processes diag files for CFSR for one year')
+print('NOTE: The paths to diags and to output are hardcoded in the script.')
+
+if len(sys.argv) < 1:
+    raise SystemExit('python fillin_year_CFSR.py <year>')
+year = int(sys.argv[1])
+
 regions=['GLOBL','TROPI','NORTH','SOUTH']
 
+dayinmon=[31,29,31,30,31,30,31,31,30,31,30,31]
 
 instrmnts=['airs', 'amsua',  'amsua',  'amsua','amsua', 'amsua', 'amsua', 'amsua', 'amsub', 'amsub', 'amsub', 'atms' , 'avhrr'  , 'avhrr', 'avhrr', 'avhrr', 'avhrr', 'cris', 'hirs2' , 'hirs3', 'hirs3', 'hirs4'   , 'hirs4' , 'iasi'   ,    'mhs',     'mhs', 'mhs' , 'mhs', 'msu', 'seviri', 'sndr', 'sndr']
 satlites=[ 'aqua', 'aqua' ,'metop-a','metop-b','n15'  , 'n16'  , 'n18'  , 'n19'  , 'n15'  , 'n16'  , 'n17'  , 'npp'  , 'metop-a', 'n15'  , 'n16'  , 'n17'  , 'n18'  , 'npp' , 'n14'   , 'n16'  , 'n17'  , 'metop-a' , 'n19'   , 'metop-a','metop-a', 'metop-b', 'n18' , 'n19', 'n14',  'm10'  , 'g08' , 'g11' ]
@@ -22,12 +31,11 @@ for nn in range(numinst):
    channels=eval(channelsname)
 
    print 'channels_name,channels=', channelsname,channels
-   year = 1999
    for region in regions:
       outfile=outputpath+'/RAD_CFSR_'+str(year)+'_'+instrmnt+'_'+satlite+'_'+region+'.nc'
       create_annual_rad(outfile, year, year, instrmnt, channels, satlite, region)
-   for month in [7]:
-     for day in range(31):
+   for month in range(12):
+     for day in range(dayinmon[month]):
        for hour in [0, 6, 12, 18]:
          date = year*1000000 + (month+1)*10000 + (day+1)*100 + hour
          print date
